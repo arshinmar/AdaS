94aaf076b8190ff1ccdcd1afdd309a5bcb713de7
Sepehr79h <sepehr79h@gmail.com>
2020-06-19 09:59:09 -0400

    Update resnet.py

--- unpackaged/models/resnet.py
+++ unpackaged/models/resnet.py
@@ -43,9 +43,15 @@ class BasicBlock(nn.Module):
         super(BasicBlock, self).__init__()
         self.conv1 = nn.Conv2d(
             in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        global convCount
+        convCount+=1
+        print(convCount,planes)
         self.bn1 = nn.BatchNorm2d(planes)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
                                stride=1, padding=1, bias=False)
+        global convCount
+        convCount+=1
+        print(convCount,planes)
         self.bn2 = nn.BatchNorm2d(planes)
 
         self.shortcut = nn.Sequential()
