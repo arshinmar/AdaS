5f8c97b6d171d0ebbde22616c217aee48f34b1b8
Sepehr79h <sepehr79h@gmail.com>
2020-06-19 17:28:23 -0400

    Update resnet.py

--- unpackaged/models/resnet.py
+++ unpackaged/models/resnet.py
@@ -127,7 +127,7 @@ class ResNet(nn.Module):
         self.layer2 = self._make_layer(block, convList[convCount], num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, convList[convCount], num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, convList[convCount], num_blocks[3], stride=2)
-        self.linear = nn.Linear(512*block.expansion, num_classes)
+        self.linear = nn.Linear(412*block.expansion, num_classes)
 
     def _make_layer(self, block, planes, num_blocks, stride):
         global convList
