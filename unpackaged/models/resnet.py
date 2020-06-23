4727b94179025a0e66b588d9927b20800353bdc5
Sepehr79h <sepehr79h@gmail.com>
2020-06-23 16:35:38 -0400

    Update resnet.py

--- unpackaged/models/resnet.py
+++ unpackaged/models/resnet.py
@@ -5,7 +5,7 @@ Copyright (c) 2017 liukuang
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
+in the Software withx restriction, including withx limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
@@ -13,12 +13,12 @@ furnished to do so, subject to the following conditions:
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+THE SOFTWARE IS PROVIDED "AS IS", WITHx WARRANTY OF ANY KIND, Ex PRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+x OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
 ResNet in PyTorch.
@@ -26,8 +26,8 @@ ResNet in PyTorch.
 For Pre-activation ResNet, see 'preact_resnet.py'.
 
 Reference:
-[1] Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun
-    Deep Residual Learning for Image Recognition. arXiv:1512.03385
+[1] Kaiming He, x iangyu Zhang, Shaoqing Ren, Jian Sun
+    Deep Residual Learning for Image Recognition. arx iv:1512.03385
 """
 import torch
 import torch.nn as nn
@@ -45,7 +45,7 @@ class BasicBlock(nn.Module):
 
         super(BasicBlock,self).__init__()
         if in_planes!=intermediate_planes:
-            ##print('shortcut_needed')
+            #print('shortcut_needed')
             stride=2
         else:
             stride=stride
@@ -70,7 +70,7 @@ class BasicBlock(nn.Module):
         self.relu=nn.ReLU()
         self.shortcut=nn.Sequential()
         if stride!=1 or in_planes!=out_planes:
-            ##print('shortcut_made')
+            #print('shortcut_made')
             self.shortcut=nn.Sequential(
                 nn.Conv2d(
                         in_planes,
@@ -85,8 +85,7 @@ class BasicBlock(nn.Module):
             )
 
     def forward(self,y):
-
-        if y.shape[2]<=4:
+        '''if x.shape[2]<=4:
             self.conv1=nn.Conv2d(
                     self.in_planes,
                     self.intermediate_planes,
@@ -106,49 +105,51 @@ class BasicBlock(nn.Module):
                 ),
                 nn.BatchNorm2d(self.out_planes),
                 nn.ReLU()
-            )
+            )'''
         x = self.conv1(y)
-        #print(out.shape,'post conv1 block')
+        #print(x.shape,'post conv1 block')
         x = self.bn1(x)
         x = self.relu(x)
         x = self.bn2(self.conv2(x))
-        #print(out.shape,'post conv2 block')
+        #print(x.shape,'post conv2 block')
         #if self.shortcut!=nn.Sequential():
             #print('shortcut_made')
+        #print(self.shortcut)
         x += self.shortcut(y)
-        #print(out.shape,'post conv3 block')
+        #print(x.shape,'post conv3 block')
         x = self.relu(x)
         return x
 
 
 class ResNet(nn.Module):
 
-    def __init__(self, block, layers,num_classes=10):
-        global convCount
+    def __init__(self, block, image_channels,num_classes=10):
         super(ResNet, self).__init__()
 
-        self.index = [64, 64, 64, 64, 64, 64, 64, 128, 128, 128, 128, 128, 128, 128, 128, 128, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 512, 512, 512, 512, 512, 512, 512]
-        #self.index = [52, 54, 54, 54, 54, 54, 54, 106, 108, 104, 108, 106, 108, 106, 106, 106, 212, 214, 208, 212, 210, 212, 210, 212, 210, 212, 210, 212, 208, 420, 418, 414, 416, 412, 414, 412]
+        #self.index  = [64, 64, 96, 96, 108, 108, 128, 128, 140, 140, 150, 150, 128, 128, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 512, 512, 512, 512, 512, 512]
+        self.index = [52, 54, 54, 54, 54, 54, 54, 106, 108, 104, 108, 106, 108, 106, 106, 106, 212, 214, 208, 212, 210, 212, 210, 212, 210, 212, 210, 212, 208, 420, 418, 414, 416, 412, 414, 412]
         self.index_temp=self.index
         #self.index_temp=[64, 64, 64, 64, 64, 64, 128, 128, 128, 128, 128, 128, 128, 128, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 512, 512, 512, 512, 512, 512]
-        self.index=self.index_temp
+        self.index =self.index_temp
         #self.in_planes = 64
-        self.conv1 = nn.Conv2d(3, self.index[0], kernel_size=7, stride=2, padding=3)
+        self.conv1 = nn.Conv2d(image_channels, self.index [0], kernel_size=7, stride=2, padding=3)
         self.bn1 = nn.BatchNorm2d(64)
-        #self.block1=self._make_block(block,self.index[0],self.index[1],self.index[2],stride=1)
+        #self.block1=self._make_block(block,self.index [0],self.index [1],self.index [2],stride=1)
         self.network=self._create_network(block)
-        self.linear=nn.Linear(self.index[len(self.index)-1],num_classes)
+        self.linear=nn.Linear(self.index [len(self.index )-1],num_classes)
         self.avgpool = nn.AdaptiveAvgPool2d(1)
         self.maxpool=nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.relu=nn.ReLU()
 
     def _create_network(self,block):
+        input_size=56
         layers=[]
         layers.append(block(self.index[0],self.index[1],self.index[2],stride=1))
         for i in range(2,len(self.index)-2,2):
-            #print(self.index[i],self.index[i+1],self.index[i+2],'for loop ',i)
-            if self.index[i]!=self.index[i+2]:
+            #print(self.index [i],self.index [i+1],self.index [i+2],'for loop ',i)
+            if self.index[i]!=self.index[i+2] and input_size>4:
                 stride=2
+                input_size=int(input_size/2)
             else:
                 stride=1
             layers.append(block(self.index[i],self.index[i+1],self.index[i+2],stride=stride))
@@ -159,34 +160,34 @@ class ResNet(nn.Module):
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.linear = nn.Linear(512*block.expansion, num_classes)
+        self.linear = nn.Linear(512*block.ex pansion, num_classes)
 
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1]*(num_blocks-1)
         layers = []
         for stride in strides:
             layers.append(block(self.in_planes, planes, stride))
-            self.in_planes = planes * block.expansion
+            self.in_planes = planes * block.ex pansion
         return nn.Sequential(*layers)'''
 
     def forward(self, x):
-        #print(self.index)
+        #print(self.index )
         x = self.conv1(x)
-        #print(out.shape, 'conv1')
+        #print(x.shape, 'conv1')
         x = self.bn1(x)
-        #print(out.shape, 'bn1')
+        #print(x.shape, 'bn1')
         x = self.relu(x)
-        #print(out.shape, 'relu')
+        #print(x.shape, 'relu')
         x = self.maxpool(x)
-        #print(out.shape, 'maxpool')
+        #print(x.shape, 'max pool')
         x = self.network(x)
-        #print(out.shape, 'post bunch of blocks')
+        #print(x.shape, 'post bunch of blocks')
         x = self.avgpool(x)
-        #print(out.shape, 'post avgpool')
+        #print(x.shape, 'post avgpool')
         x = x.view(x.size(0), -1)
-        #print(out.shape, 'post reshaping')
+        #print(x.shape, 'post reshaping')
         x = self.linear(x)
-        #print(out.shape, 'post fc')
+        #print(x.shape, 'post fc')
         return x
 
 
@@ -194,8 +195,8 @@ def ResNet18(num_classes: int = 10):
     return ResNet(BasicBlock, [2, 2, 2, 2], num_classes=num_classes)
 
 
-def ResNet34(num_classes: int = 10):
-    return ResNet(BasicBlock, [3, 4, 6, 3], num_classes=num_classes)
+def ResNet34(num_classes = 10):
+    return ResNet(BasicBlock, 3, num_classes=10)
 
 
 def ResNet50(num_classes: int = 10):
@@ -212,7 +213,8 @@ def ResNet152(num_classes: int = 10):
 
 def test():
     net = ResNet34()
+    ##print(net)
     y = net(torch.randn(1, 3, 224, 224))
     #print(y.size())
 
-#test()
+test()
