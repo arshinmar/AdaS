24a928288a86b1f8bd6ac6c23b9cfb3aef994a52
Sepehr79h <sepehr79h@gmail.com>
2020-06-23 13:23:38 -0400

    resnet updates

--- unpackaged/models/resnet.py
+++ unpackaged/models/resnet.py
@@ -35,124 +35,156 @@ import torch.nn.functional as F
 import numpy as np
 
 convCount=0
-convList=[52,54,54,54,54,54,54,106,108,104,108,106,108,106,106,106,212,214,208,212,210,212,210,212,210,212,210,212,208,420,418,414,416,412,414,412,8,8,8,8,8,8,8,8,8,8,8,8,8,8,10,10]
-#convList=[52,52,54,54,54,54,54,54,106,108,104,108,106,108,106,106,106,212,214,208,212,210,212,210,212,210,212,210,212,208,420,418,414,416,412,414,412,8,8,8,8,8,8,8,8,8,8,8,8,8,8,10,10]
 
 class BasicBlock(nn.Module):
-    expansion = 1
-    def __init__(self, in_planes, planes, stride=1):
-        global convCount
-        global convList
-        super(BasicBlock, self).__init__()
-        self.conv1 = nn.Conv2d(
-            in_planes, convList[convCount], kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(convList[convCount])
-        print(convCount,convList[convCount])
-        convCount+=1
-        self.conv2 = nn.Conv2d(convList[convCount], convList[convCount], kernel_size=3,
-                               stride=1, padding=1, bias=False)
 
-        self.bn2 = nn.BatchNorm2d(convList[convCount])
-        print(convCount,convList[convCount])
-        convCount+=1
+    def __init__(self,in_planes,intermediate_planes, out_planes,stride=1):
+        self.in_planes=in_planes
+        self.intermediate_planes=intermediate_planes
+        self.out_planes=out_planes
 
-        self.shortcut = nn.Sequential()
-        if stride != 1 or in_planes != self.expansion*convList[convCount]:
-            #print('case',in_planes)
-            #print('case',convList[convCount])
-            print('shortcut')
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion*convList[convCount],
-                          kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion*convList[convCount])
+        super(BasicBlock,self).__init__()
+        if in_planes!=intermediate_planes:
+            print('shortcut_needed')
+            stride=2
+        else:
+            stride=stride
+        self.conv1=nn.Conv2d(
+                in_planes,
+                intermediate_planes,
+                kernel_size=3,
+                stride=stride,
+                padding=1,
+                bias=False
+        )
+        self.bn1=nn.BatchNorm2d(intermediate_planes)
+        self.conv2=nn.Conv2d(
+                intermediate_planes,
+                out_planes,
+                kernel_size=3,
+                stride=1,
+                padding=1,
+                bias=False
+        )
+        self.bn2=nn.BatchNorm2d(out_planes)
+        self.relu=nn.ReLU()
+        self.shortcut=nn.Sequential()
+        if stride!=1 or in_planes!=out_planes:
+            #print('shortcut_made')
+            self.shortcut=nn.Sequential(
+                nn.Conv2d(
+                        in_planes,
+                        out_planes,
+                        kernel_size=3,
+                        stride=stride,
+                        padding=1,
+                        bias=False
+                ),
+                nn.BatchNorm2d(out_planes),
+                nn.ReLU()
             )
-            print(convCount,convList[convCount])
-            convCount+=1
 
-
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
+    def forward(self,x):
+        if x.shape[2]<=4:
+            self.conv1=nn.Conv2d(
+                    self.in_planes,
+                    self.intermediate_planes,
+                    kernel_size=3,
+                    stride=1,
+                    padding=1,
+                    bias=False
+            )
+            self.shortcut=nn.Sequential(
+                nn.Conv2d(
+                        self.in_planes,
+                        self.out_planes,
+                        kernel_size=3,
+                        stride=1,
+                        padding=1,
+                        bias=False
+                ),
+                nn.BatchNorm2d(self.out_planes),
+                nn.ReLU()
+            )
+        out = self.conv1(x)
+        print(out.shape,'post conv1 block')
+        out = self.bn1(out)
+        out = self.relu(out)
         out = self.bn2(self.conv2(out))
+        print(out.shape,'post conv2 block')
+        if self.shortcut!=nn.Sequential():
+            print('shortcut_made')
         out += self.shortcut(x)
-        out = F.relu(out)
-        return out
-
-
-class Bottleneck(nn.Module):
-    expansion = 4
-
-    def __init__(self, in_planes, planes, stride=1):
-        super(Bottleneck, self).__init__()
-        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
-                               stride=stride, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.conv3 = nn.Conv2d(planes, self.expansion *
-                               planes, kernel_size=1, bias=False)
-        self.bn3 = nn.BatchNorm2d(self.expansion*planes)
-
-        self.shortcut = nn.Sequential()
-        if stride != 1 or in_planes != self.expansion*planes:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion*planes,
-                          kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion*planes)
-            )
-
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = F.relu(self.bn2(self.conv2(out)))
-        out = self.bn3(self.conv3(out))
-        out += self.shortcut(x)
-        out = F.relu(out)
+        print(out.shape,'post conv3 block')
+        out = self.relu(out)
         return out
 
 
 class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10):
+
+    def __init__(self, block, image_channels,num_classes=10):
         global convCount
-        global convList
         super(ResNet, self).__init__()
 
-        self.index_info_conv = []
+        self.index = [64, 64, 96, 96, 108, 108, 128, 128, 140, 140, 150, 150, 128, 128, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 512, 512, 512, 512, 512, 512]
+        self.index_temp=self.index
+        #self.index_temp=[64, 64, 64, 64, 64, 64, 128, 128, 128, 128, 128, 128, 128, 128, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 512, 512, 512, 512, 512, 512]
+        self.index=self.index_temp
+        #self.in_planes = 64
+        self.conv1 = nn.Conv2d(image_channels, self.index[0], kernel_size=7, stride=2, padding=3)
+        self.bn1 = nn.BatchNorm2d(64)
+        #self.block1=self._make_block(block,self.index[0],self.index[1],self.index[2],stride=1)
+        self.network=self._create_network(BasicBlock)
+        self.linear=nn.Linear(self.index[len(self.index)-1],num_classes)
+        self.avgpool = nn.AdaptiveAvgPool2d(1)
+        self.maxpool=nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.relu=nn.ReLU()
 
-        self.in_planes = convList[convCount]
-        print('self.in_planes:',self.in_planes)
-        self.conv1 = nn.Conv2d(3, convList[convCount], kernel_size=3,
-                               stride=1, padding=1, bias=False)
+    def _create_network(self,block):
+        layers=[]
+        layers.append(block(self.index[0],self.index[1],self.index[2],stride=1))
+        for i in range(2,len(self.index)-2,2):
+            print(self.index[i],self.index[i+1],self.index[i+2],'for loop ',i)
+            if self.index[i]!=self.index[i+2]:
+                stride=2
+            else:
+                stride=1
+            layers.append(block(self.index[i],self.index[i+1],self.index[i+2],stride=stride))
+        return nn.Sequential(*layers)
 
-        self.bn1 = nn.BatchNorm2d(convList[convCount])
-
-        print(convCount,convList[convCount])
-        convCount+=1
-
-        self.layer1 = self._make_layer(block, convList[convCount], num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, convList[convCount], num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, convList[convCount], num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, convList[convCount], num_blocks[3], stride=2)
-        self.linear = nn.Linear(412*block.expansion, num_classes)
+    '''
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.linear = nn.Linear(512*block.expansion, num_classes)
 
     def _make_layer(self, block, planes, num_blocks, stride):
-        global convList
-        global convCount
         strides = [stride] + [1]*(num_blocks-1)
         layers = []
         for stride in strides:
             layers.append(block(self.in_planes, planes, stride))
-            self.in_planes = convList[convCount] * block.expansion
-        return nn.Sequential(*layers)
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)'''
 
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        #out.shape=(52,whatever,whatever)
-        out = self.layer1(out)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
-        out = F.avg_pool2d(out, 4)
+        print(self.index)
+        out = self.conv1(x)
+        print(out.shape, 'conv1')
+        out = self.bn1(out)
+        print(out.shape, 'bn1')
+        out = self.relu(out)
+        print(out.shape, 'relu')
+        out = self.maxpool(out)
+        print(out.shape, 'maxpool')
+        out = self.network(out)
+        print(out.shape, 'post bunch of blocks')
+        out = self.avgpool(out)
+        print(out.shape, 'post avgpool')
         out = out.view(out.size(0), -1)
+        print(out.shape, 'post reshaping')
         out = self.linear(out)
+        print(out.shape, 'post fc')
         return out
 
 
@@ -160,8 +192,8 @@ def ResNet18(num_classes: int = 10):
     return ResNet(BasicBlock, [2, 2, 2, 2], num_classes=num_classes)
 
 
-def ResNet34(num_classes: int = 10):
-    return ResNet(BasicBlock, [3, 4, 6, 3], num_classes=num_classes)
+def ResNet34(image_channels: int=3, num_classes: int = 10):
+    return ResNet(BasicBlock, image_channels, num_classes=num_classes)
 
 
 def ResNet50(num_classes: int = 10):
@@ -177,8 +209,8 @@ def ResNet152(num_classes: int = 10):
 
 
 def test():
-    net = ResNet18()
-    y = net(torch.randn(1, 3, 32, 32))
+    net = ResNet34()
+    y = net(torch.randn(1, 3, 224, 224))
     print(y.size())
 
-#test()
+test()
